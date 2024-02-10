9120560b42cef906eff0f7b0bd7eda41c36f786f
Jason Park <jjpark987@gmail.com>
2024-02-09 16:07:01 -0800

    Configure sidekiq.yml

---
+++ bin/render-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo 'Running render-build.sh...'
+# exit on error
+set -e
+
+# verbose mode
+set -x
+
+# builds the front end code
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# builds the back end code
+bundle install
+bundle exec rake db:migrate
+
+echo '...finished running render-build.sh'
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,8 @@
 Rails.application.reloader.to_prepare do
+    sidekiq_config = YAML.load_file("#{Rails.root}/config/sidekiq.yml")
+    redis_url = sidekiq_config[Rails.env]['url']
+    redis_namespace = sidekiq_config[Rails.env]['namespace']
+    
     Sidekiq.configure_server do |config|
         config.redis = { url: 'redis://localhost:6379/0'  }
         schedule_file = "config/schedule.yml"
