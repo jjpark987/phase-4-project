8725220be8a9b21f1fb469ed9ade612186531346
Jason Park <jjpark987@gmail.com>
2024-02-14 17:04:08 -0800

    Comment out seed lines in render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -15,7 +15,7 @@ cp -a client/build/. public/
 # builds the back end code
 bundle install
 bundle exec rake db:migrate
-bundle exec rake db:seed
-ruby bin/store_gif_blob_for_all_exercises.rb
+# bundle exec rake db:seed
+# ruby bin/store_gif_blob_for_all_exercises.rb
 
 echo '...finished running render-build.sh'
