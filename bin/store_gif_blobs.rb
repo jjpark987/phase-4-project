2f97a3eeb859470d48d8f1d94443deda4913abee
Jason Park <jjpark987@gmail.com>
2024-02-15 20:03:04 -0800

    Revert store_gif_blobs to before the resizing attempts

--- bin/store_gif_blobs.rb
+++ bin/store_gif_blobs.rb
@@ -4,9 +4,6 @@ def store_gif_blobs
     Exercise.all.each do |exercise|
         begin
             downloaded_gif = URI.open(exercise.gif_url)
-
-            processed_gif = ImageProcessing::MiniMagick.source(downloaded_gif).resize_to_fit(5, 5)
-
             exercise.gif.attach(io: downloaded_gif, filename: "#{exercise.name}_gif.gif")
             puts "Successfully attached gif for #{exercise.name}"
         rescue StandardError => e
