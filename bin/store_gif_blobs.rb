125efbf37278aeacffaf652a5292b74c848bb59a
Jason Park <jjpark987@gmail.com>
2024-02-15 18:00:40 -0800

    Update store_gif_blobs.rb

--- bin/store_gif_blobs.rb
+++ bin/store_gif_blobs.rb
@@ -3,16 +3,12 @@ require_relative '../config/environment'
 def store_gif_blobs
     Exercise.all.each do |exercise|
         begin
-            if !exercise.gif.attached?
-                downloaded_gif = URI.open(exercise.gif_url)
+            downloaded_gif = URI.open(exercise.gif_url)
 
-                processed_gif = ImageProcessing::MiniMagick.source(downloaded_gif).resize_to_fit(5, 5)
+            processed_gif = ImageProcessing::MiniMagick.source(downloaded_gif).resize_to_fit(5, 5)
 
-                exercise.gif.attach(io: downloaded_gif, filename: "#{exercise.name}_gif.gif")
-                puts "Successfully attached gif for #{exercise.name}"
-            else
-                puts "Skipped #{exercise.name}"
-            end
+            exercise.gif.attach(io: downloaded_gif, filename: "#{exercise.name}_gif.gif")
+            puts "Successfully attached gif for #{exercise.name}"
         rescue StandardError => e
             puts "Error attaching gif for #{exercise.name}: #{e.message}"
         end
