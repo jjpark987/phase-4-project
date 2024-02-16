834c337fe84e8eeb16afa12458039ea653fbe867
Jason Park <jjpark987@gmail.com>
2024-02-15 20:15:29 -0800

    Use mini_magick to resize gif to 75%

--- bin/store_gif_blobs.rb
+++ bin/store_gif_blobs.rb
@@ -4,7 +4,10 @@ def store_gif_blobs
     Exercise.all.each do |exercise|
         begin
             downloaded_gif = URI.open(exercise.gif_url)
-            exercise.gif.attach(io: downloaded_gif, filename: "#{exercise.name}_gif.gif")
+            image = MiniMagick::Image.read(downloaded_gif)
+            image.resize "75%"
+            resized_gif_io = StringIO.new(image.to_blob)
+            exercise.gif.attach(io: resized_gif_io, filename: "#{exercise.name}_gif.gif")
             puts "Successfully attached gif for #{exercise.name}"
         rescue StandardError => e
             puts "Error attaching gif for #{exercise.name}: #{e.message}"
